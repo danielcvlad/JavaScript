8607dff549a45bdee2c7048ab89ca95edbe8ac66
Daniel Vlad <danielcvlad@gmail.com>
2020-03-19 12:42:41 +0200

    windows

---
+++ Coffee Shop website project/project/win_coffee_complete and setup/setup/js/app.js
@@ -0,0 +1,65 @@
+// hide preloader
+
+// window event listener
+eventListeners();
+function eventListeners() {
+  const ui = new UI();
+  //preloader
+  window.addEventListener("load", function() {
+    ui.hidePreloader();
+  });
+  // nav btn
+  document.querySelector(".navBtn").addEventListener("click", function() {
+    ui.showNav();
+  });
+  //control background video
+  document
+    .querySelector(".video__switch")
+    .addEventListener("click", function() {
+      ui.videoControls();
+    });
+}
+// submit the form
+document
+  .querySelector(".drink-form")
+  .addEventListener("submit", function(event) {
+    event.preventDefault();
+    const name = document.querySelector(".input-name").value;
+    const lastName = document.querySelector(".input-lastname").value;
+    const email = document.querySelector(".input-email").value;
+
+    let value = ui.checkEmpty(name, lastName, email);
+    console.log(value);
+  });
+//constructor function
+function UI() {}
+
+// hide preloader
+UI.prototype.hidePreloader = function() {
+  document.querySelector(".preloader").style.display = "none";
+};
+// show Nav
+UI.prototype.showNav = function() {
+  document.querySelector(".nav").classList.toggle("nav--show");
+};
+// play/pause the vidoe
+UI.prototype.videoControls = function() {
+  let btn = document.querySelector(".video__switch-btn");
+  if (!btn.classList.contains("btnSlide")) {
+    btn.classList.add("btnSlide");
+    document.querySelector(".video__item").pause();
+  } else {
+    btn.classList.remove("btnSlide");
+    document.querySelector(".video__item").play();
+  }
+};
+// check for empty values
+UI.prototype.checkEmpty = function(name, lastname, email) {
+  let result;
+  if (name === "" || lastname === "" || email === "") {
+    result = false;
+  } else {
+    result = true;
+  }
+  return result;
+};
