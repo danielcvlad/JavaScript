61bcd62c9f79dae5a9e13d6d0b46a2d4b9147a55
Daniel Vlad <danielcvlad@gmail.com>
2020-03-20 11:00:13 +0200

    updated website

--- Coffee Shop website project/project/win_coffee_complete and setup/setup/js/app.js
+++ Coffee Shop website project/project/win_coffee_complete and setup/setup/js/app.js
@@ -1,10 +1,10 @@
-// hide preloader
+// A base class is defined using the new reserved 'class' keyword
 
-// window event listener
+// event Listeners
 eventListeners();
 function eventListeners() {
   const ui = new UI();
-  //preloader
+  // preloader
   window.addEventListener("load", function() {
     ui.hidePreloader();
   });
@@ -12,37 +12,53 @@ function eventListeners() {
   document.querySelector(".navBtn").addEventListener("click", function() {
     ui.showNav();
   });
-  //control background video
+  // control the video
   document
     .querySelector(".video__switch")
     .addEventListener("click", function() {
       ui.videoControls();
     });
-}
-// submit the form
-document
-  .querySelector(".drink-form")
-  .addEventListener("submit", function(event) {
-    event.preventDefault();
-    const name = document.querySelector(".input-name").value;
-    const lastName = document.querySelector(".input-lastname").value;
-    const email = document.querySelector(".input-email").value;
+  // submit the form
+  document
+    .querySelector(".drink-form")
+    .addEventListener("submit", function(event) {
+      event.preventDefault();
+      const name = document.querySelector(".input-name").value;
+      const lastName = document.querySelector(".input-lastname").value;
+      const email = document.querySelector(".input-email").value;
 
-    let value = ui.checkEmpty(name, lastName, email);
-    console.log(value);
+      let value = ui.checkEmpty(name, lastName, email);
+      console.log(value);
 
-    if (value) {
-      let customer = new customer(name, lastName, email);
-      console.log(customer);
-      ui.addCustomer(customer);
-      ui.showFeedback("customer added to the list", "success");
-      ui.clearFields();
-    } else {
-      ui.showFeedback("some form values empty", "error");
-    }
+      if (value) {
+        let customer = new Customer(name, lastName, email);
+        console.log(customer);
+        ui.addCustomer(customer);
+        ui.showFeedback("custumer added to the list", "success");
+        ui.clearFields();
+      } else {
+        ui.showFeedback("some form values empty", "error");
+      }
+    });
+  // display modal
+  const links = document.querySelectorAll(".work-item__icon");
+
+  links.forEach(function(item) {
+    item.addEventListener("click", function(event) {
+      ui.showModal(event);
+    });
   });
+  // hide modal
+  document
+    .querySelector(".work-modal__close")
+    .addEventListener("click", function() {
+      ui.closeModal();
+    });
+}
+
 //constructor function
 function UI() {}
+
 // hide preloader
 UI.prototype.hidePreloader = function() {
   document.querySelector(".preloader").style.display = "none";
@@ -51,7 +67,7 @@ UI.prototype.hidePreloader = function() {
 UI.prototype.showNav = function() {
   document.querySelector(".nav").classList.toggle("nav--show");
 };
-// play/pause the video
+// play/pause the vidoe
 UI.prototype.videoControls = function() {
   let btn = document.querySelector(".video__switch-btn");
   if (!btn.classList.contains("btnSlide")) {
@@ -72,42 +88,63 @@ UI.prototype.checkEmpty = function(name, lastname, email) {
   }
   return result;
 };
+
 UI.prototype.showFeedback = function(text, type) {
   const feedback = document.querySelector(".drink-form__feedback");
   if (type === "success") {
     feedback.classList.add("success");
     feedback.innerText = text;
-    this.remove("success");
+    this.removeAlert("success");
   } else if (type === "error") {
     feedback.classList.add("error");
     feedback.innerText = text;
     this.removeAlert("error");
   }
 };
-//remove alert
+// remove alert
 UI.prototype.removeAlert = function(type) {
   setTimeout(function() {
     document.querySelector(".drink-form__feedback").classList.remove(type);
   }, 3000);
 };
-//add customer
+// add customer
 UI.prototype.addCustomer = function(customer) {
   const images = [1, 2, 3, 4, 5];
   let random = Math.floor(Math.random() * images.length);
   const div = document.createElement("div");
   div.classList.add("person");
-  div.innerHTML = `<img
-                src="img/person-${random}.jpeg"
-                alt="person"
-                class="person__thumbnail"
-              />
-              <h4 class="person__name">${customer.name}</h4>
-              <h4 class="person__last-name">${customer.lastname}</h4>`;
+  div.innerHTML = `<img src="img/person-${random}.jpeg" alt="person" class="person__thumbnail">
+            <h4 class="person__name">${customer.name}</h4>
+            <h4 class="person__last-name">${customer.lastname}</h4>`;
   document.querySelector(".drink-card__list").appendChild(div);
 };
-// clear fields in form after submit
+// clear fields
 UI.prototype.clearFields = function() {
   document.querySelector(".input-name").value = "";
   document.querySelector(".input-lastname").value = "";
   document.querySelector(".input-email").value = "";
 };
+// show modal
+
+UI.prototype.showModal = function(event) {
+  event.preventDefault();
+  if (event.target.parentElement.classList.contains("work-item__icon")) {
+    let id = event.target.parentElement.dataset.id;
+
+    const modal = document.querySelector(".work-modal");
+    const modalItem = document.querySelector(".work-modal__item");
+
+    modal.classList.add("work-modal--show");
+    modalItem.style.backgroundImage = `url(img/work-${id}.jpeg)`;
+  }
+};
+// hide modal
+
+UI.prototype.closeModal = function() {
+  document.querySelector(".work-modal").classList.remove("work-modal--show");
+};
+
+// customer
+function Customer(name, lastname, email) {
+  (this.name = name), (this.lastname = lastname), (this.email = email);
+}
