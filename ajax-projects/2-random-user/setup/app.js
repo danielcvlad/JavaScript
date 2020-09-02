fcab3e78ee8120c08cf833891e7da1f032583189
Daniel Vlad <danielcvlad@gmail.com>
2020-09-02 17:31:00 +0300

    finished project

--- ajax-projects/2-random-user/setup/app.js
+++ ajax-projects/2-random-user/setup/app.js
@@ -1,16 +1,14 @@
 import get from "./utils/getElement.js";
-import getUser from "./utils/fetchUser.js";
 
-const img = get(".user-img");
-const title = get(".user-title");
-const value = get(".user-value");
+import getUser from "./utils/fetchUser.js";
+import displayUser from "./utils/displayUser.js";
+
 const btn = get(".btn");
-const btns = [...document.querySelectorAll(".icon")];
 
 const showUser = async () => {
   //get user from api
   const person = await getUser();
-  console.log(person);
+  displayUser(person);
   //display user
 };
 
